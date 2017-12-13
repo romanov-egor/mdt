bd35b01089db4c86937c212db668642aafd602ff
Home <Home@liferay.com>
2017-12-13 12:13:31 +0300

    date format changed 2

--- src/main/java/ru/romanov/mydailytasks/service/impl/TaskServiceImpl.java
+++ src/main/java/ru/romanov/mydailytasks/service/impl/TaskServiceImpl.java
@@ -12,7 +12,7 @@ import ru.romanov.mydailytasks.web.util.Converter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.List;s
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
